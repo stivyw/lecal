84460eaf5f2da142a7b08aed2157cf4d4d0e6686
stivyw <stivyw@gmail.com>
2015-05-15 01:16:40 -0300

    Learning about gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,8 @@ var gulp = require("gulp");
 var shell = require("gulp-shell");
 var elixir = require("laravel-elixir");
 
-elixir.extend("speak", function(message) {
-
-    gulp.task("speak", function() {
-        gulp.src("").pipe(shell("echo " + message));
-    });
-
-    return this.queueTask("speak");
-
- });
-
-elixir(function(mix) {
-    mix.speak("Tea, Earl Grey, Hot");
+elixir(function (mix) {
+  mix.scripts([
+    "app.js"
+  ], "dist/all.js", "src/js/");
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
   * @require ./lecal.js
   * @require ./services/auth.js
  */
+ require('./lecal');
  App = angular.module('App', ['lecal']);
  App.controller('Ctrl', ['Auth', function (Auth) {
  	console.log(Auth.getHello());
