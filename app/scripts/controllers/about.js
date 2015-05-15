75200089dde1d8bf8c3a70010317540bcf208ea5
stivyw <stivyw@gmail.com>
2015-05-15 02:53:31 -0300

    Master

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lecalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lecalApp
+ */
+angular.module('lecalApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lecalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lecalApp
+ */
+angular.module('lecalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
