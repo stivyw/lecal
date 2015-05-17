44e9cd59dd1c8a8a4e062ca0d4d9a72b4405b603
stivyw <stivyw@gmail.com>
2015-05-17 13:20:26 -0300

    Tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 /**
-  * @require ./lecal.js
-  * @require ./services/auth.js
- */
- 
- angular.module('App', ['lecal', 'ngResource'])
- 
- .controller('Ctrl', ['Auth', 'Model', function (Auth, Model) {
- 	var model = Model;
- 	console.log(Model.hello);
- 	Model.get({id:2}), function (x) {
- 		console.log(x);
- 	};
- }]);
+ * @require ./lecal.js
+ * @require ./services/auth.js
+*/
+
+angular.module('App', ['lecal', 'ngResource'])
+
+.controller('Ctrl', ['Auth', 'Model', function (Auth, Model) {
+	var model = Model;
+	console.log(Model.hello);
+	Model.get({id:2}), function (x) {
+		console.log(x);
+	};
+}]);
