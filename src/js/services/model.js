be213fe33c02aec59a618ad514e3122026c0af3c
stivyw <stivyw@gmail.com>
2015-05-19 05:27:41 -0300

    Zoando

--- src/js/services/model.js
+++ src/js/services/model.js
@@ -1,14 +1,21 @@
-
 services.provider('Model', [function() {
-		
 		this.base = 'http://localhost/api/v1/';
 		this.action = '';
+
 		this.$get = ['$resource', function($resource) {
 
-				var model = $resource(this.base + this.action + '/' + ':id', {id: '@id'});
-				//model.prototype
-				return model;
+				//var model = $resource(this.base + this.action + '/' + ':id', {id: '@id'});
+				var name, action, base = this.base;
+				var model = $resource(action, {id: '@id'});
+
+
+				return function(modelName, ext){
+					name = modelName;
+					action = base + this.action + '/' + ':id';
+					console.log(action);
+					var model = $resource(action, {id: '@id'});
+					typeof ext === 'object' && angular.extend(model.prototype, ext);
+					return model;
+				};
 		}];
-
-
 }]);
